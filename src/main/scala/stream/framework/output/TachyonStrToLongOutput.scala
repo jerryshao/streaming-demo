88021aaeb50f58f8c79e8fd5f205b682cf37ec69
jerryshao <saisai.shao@intel.com>
2013-04-22 15:54:11 +0800

    change tachyon output

--- src/main/scala/stream/framework/output/TachyonStrToLongOutput.scala
+++ src/main/scala/stream/framework/output/TachyonStrToLongOutput.scala
@@ -16,43 +16,6 @@ class TachyonStrToLongOutput extends AbstractTachyonEventOutput {
   @transient lazy val table = getTachyonTable(4)
   
   override def output(stream: DStream[_]) {
-//    stream.foreach(r => {
-//      val rows = r.collect()
-//      
-//      //first create metaColumn
-//      createMetaCol(rows.length)
-//      
-//      val currTime = System.currentTimeMillis() / 1000
-//      
-//      //create column builder for 3 data column
-//      val colBuilders = Array(new LongColumnBuilder, 
-//          new StringColumnBuilder, new LongColumnBuilder)
-//      colBuilders.foreach(_.initialize(rows.length))
-//      
-//      // build the column
-//      rows.foreach(r => {
-//        val newRow = r.asInstanceOf[(String, Long)]
-//        
-//        colBuilders(0).asInstanceOf[LongColumnBuilder].append(currTime)
-//        colBuilders(1).asInstanceOf[StringColumnBuilder].append(new Text(newRow._1))
-//        colBuilders(2).asInstanceOf[LongColumnBuilder].append(newRow._2)
-//      })
-//      
-//      //write to partition
-//      colBuilders.zipWithIndex.foreach(c => {
-//        val col = table.getRawColumn(c._2 + 1)
-//        col.createPartition(partition)
-//        
-//        val file = col.getPartition(partition)
-//        val os = file.createOutStream(OpType.WRITE_CACHE)
-//        
-//        val buf = c._1.build
-//        os.write(buf)
-//        os.close()
-//      })
-//      
-//      partition += 1
-//    })
     stream.mapPartitionsWithTimeIndexId((time, index, id, iter) => {
       val colBuilders = Array(new LongColumnBuilder, new StringColumnBuilder, 
           new LongColumnBuilder)
